8c688e4ece9923cb1e91d1ccc459ed475d9a3287
transfaeries <transfaeries@gmail.com>
2025-05-30 16:49:28 -0400

    set up for running tests

--- faediscord.py
+++ faediscord.py
@@ -455,5 +455,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 # instantiate and run the bot
-client = Faebot(intents=intents)
-client.run(os.getenv("DISCORD_TOKEN", ""))
+if __name__ == "__main__":
+    client = Faebot(intents=intents)
+    client.run(os.getenv("DISCORD_TOKEN", ""))
